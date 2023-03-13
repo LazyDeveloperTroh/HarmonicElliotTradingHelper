8adef407cdbaf84298bb1f9b92bdf77f246b0873
fianc <dhxofla123@naver.com>
2023-03-13 16:23:08 +0900

    엘리어트 웨이브 & 하모닉 패턴 트레이딩 도움 시스템 취미&개발 공부 모두를 위한 시스템으로 기능에 비해 오버스펙일 수 있음

---
+++ src/test/java/com/heth/harmonicelliottradinghelper/HarmonicElliotTradingHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heth.harmonicelliottradinghelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonicElliotTradingHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
