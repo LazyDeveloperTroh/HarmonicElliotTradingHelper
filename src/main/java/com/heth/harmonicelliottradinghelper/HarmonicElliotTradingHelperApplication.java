a6e995c6e4b524e3ce39fe8ae4f31722fa221d85
troh <dhxofla91@gmail.com>
2024-02-21 20:08:47 +0900

    Initial commit

---
+++ src/main/java/com/heth/harmonicelliottradinghelper/HarmonicElliotTradingHelperApplication.java
@@ -0,0 +1,13 @@
+package com.heth.harmonicelliottradinghelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonicElliotTradingHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmonicElliotTradingHelperApplication.class, args);
+    }
+
+}
