1a8d8033cad3956d01590e630cd60bf24604cec7
troh <dhxofla91@gmail.com>
2024-03-18 10:16:29 +0900

    HEADER 레이아웃 조정

--- src/main/frontend/src/components/Header/Header.js
+++ src/main/frontend/src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import Logo from './Logo'
-import Profile from "./Profile";
+import {Grid} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 
 function Header() {
-    return <header style={{height: "5%", display: "flex", justifyContent: "start"}}>
-        <Logo />
-        <Profile/>
-    </header>
+    return <>
+            <Grid item md={2}>
+                <div style={{display: "flex", height: "100%", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{width: "20%"}} src="/logo192.png" ></img>
+                    <p style={{width: "40%", fontSize: "1.5rem", fontWeight: "bold" }}>H E T H</p>
+                </div>
+            </Grid>
+            <Grid item md={12}>
+                <Avatar>OH</Avatar>
+            </Grid>
+    </>;
 }
 
 export default Header;
--- src/main/frontend/src/components/Header/Profile.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import Avatar from "@mui/material/Avatar"
-function Profile() {
-    return <div style={{flexBasis: "90%"}}>
-        <Avatar>OH</Avatar>
-    </div>
-}
-
-export default Profile;
