cc34c417fec11d1c966919df99dacfb8ec3b35fb
troh <dhxofla91@gmail.com>
2024-04-07 00:11:36 +0900

    JpaAuditor 유저설정

--- src/main/java/com/heth/harmonicelliottradinghelper/HarmonicElliotTradingHelperApplication.java
+++ src/main/java/com/heth/harmonicelliottradinghelper/HarmonicElliotTradingHelperApplication.java
@@ -2,12 +2,22 @@ package com.heth.harmonicelliottradinghelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HarmonicElliotTradingHelperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HarmonicElliotTradingHelperApplication.class, args);
     }
 
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("troh");
+    }
 }
