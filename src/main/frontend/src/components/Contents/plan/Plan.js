13b99109758c938569589cb86bcdbef30dbe3117
troh <dhxofla91@gmail.com>
2024-04-13 23:16:57 +0900

    매매일지 form title, ticker, position, basis, result

--- src/main/frontend/src/components/Contents/plan/Plan.js
+++ src/main/frontend/src/components/Contents/plan/Plan.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {
+    Button,
     Checkbox,
     FormControl,
     FormControlLabel,
@@ -14,74 +15,125 @@ import {
     TextField
 } from "@mui/material";
 import ChartExplainList from "./chart/ChartExplainList";
-
+import './Plan.css';
+import {useState} from "react";
 
 function Plan() {
+    const [title, setTitle] = useState("");
+    const [ticker, setTicker] = useState("BTCUSDT");
+    const [position, setPosition] = useState("LONG");
+    const [basisList, setBasisList] = useState([
+        {name: "카운팅",code: "COUNTING", check: false},
+        {name: "다이버전스",code: "DIVERGENCE", check: false},
+        {name: "매물대",code: "VOLUME PROFILE", check: false},
+        {name: "피보나치",code: "FIBONACCI RATIO", check: true},
+        {name: "지지/저항",code: "SUPPORT/RESISTANCE", check: false},
+        {name: "추세",code: "TREND", check: false},
+        {name: "캔들",code: "CANDLE", check: false}
+    ]);
+    const [result, setResult] = useState('WAITING');
+
+    const [tickerOpen, setTickerOpen] = useState(false);
+    const [positionOpen, setPositionOpen] = useState(false);
+    const save = () => {
+        console.log(title);
+        console.log(ticker);
+        console.log(position);
+        console.log(basisList);
+        console.log(result);
+    }
+
+
     return (
         <Paper component={"form"} elevation={3} sx={{height: "98%", padding: "inherit"}}>
+            <div className={"float-button-container"}>
+                <Button variant="contained" onClick={save}>저장하기</Button>
+            </div>
             <Grid container spacing={2}>
                 <Grid item md={9}>
                     <InputLabel id="title-label" sx={{fontWeight: "bold", padding: "0.5rem 0rem"}}>타이틀</InputLabel>
-                    <TextField labelId="title-label"
-                               InputLabelProps={{shrink: true}}
-                               fullWidth
-                               size={"small"}
-                               placeholder={"2024/04/01 $70,500 웻지 숏 전략"}/>
+                    <TextField
+                        fullWidth
+                        size={"small"}
+                        placeholder={"2024/04/01 $70,500 웻지 숏 전략"}
+                        defaultValue={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
                 </Grid>
                 <Grid item container md={12} columnSpacing={3}>
                     <Grid item md={1}>
                         <InputLabel id="ticker-label"
                                     sx={{fontWeight: "bold", padding: "0.5rem 0rem"}}>Ticker</InputLabel>
                         <Select
-                            labelId="ticker-label"
-                            InputLabelProps={{shrink: true}}
-                            id="demo-simple-select"
-                            value="10"
+                            value={ticker}
                             size={"small"}
+                            open={tickerOpen}
+                            onClose={() => setTickerOpen(false)}
+                            onOpen={() => setTickerOpen(true)}
+                            onChange={(e) => setTicker(e.target.value)}
                         >
-                            <MenuItem value={10}>BTCUSDT</MenuItem>
-                            <MenuItem value={20}>ETHUSDT</MenuItem>
-                            <MenuItem value={30}>XRPUSDT</MenuItem>
+                            <MenuItem value={"BTCUSDT"}>BTCUSDT</MenuItem>
+                            <MenuItem value={"ETHUSDT"}>ETHUSDT</MenuItem>
+                            <MenuItem value={"XRPUSDT"}>XRPUSDT</MenuItem>
                         </Select>
                     </Grid>
                     <Grid item md={1}>
                         <InputLabel id="position-label"
                                     sx={{fontWeight: "bold", padding: "0.5rem 0rem"}}>Position</InputLabel>
                         <Select
-                            labelId="position-label"
-                            InputLabelProps={{shrink: true}}
-                            id="demo-simple-select"
-                            value="LONG"
+                            value={position}
                             size={"small"}
+                            open={positionOpen}
+                            onClose={() => setPositionOpen(false)}
+                            onOpen={() => setPositionOpen(true)}
+                            onChange={(e) => setPosition(e.target.value)}
                         >
                             <MenuItem value={"LONG"}>LONG</MenuItem>
                             <MenuItem value={"SHORT"}>SHORT</MenuItem>
                         </Select>
                     </Grid>
                     <Grid item md={5.5}>
-                        <InputLabel id="demo-simple-select-label" sx={{fontWeight: "bold", padding: "0.5rem 0rem"}}>진입
-                            근거</InputLabel>
+                        <InputLabel id="basis-label" sx={{fontWeight: "bold", padding: "0.5rem 0rem"}}>진입근거</InputLabel>
                         <FormControl style={{flexDirection: "row"}}>
-                            <FormControlLabel control={<Checkbox/>} label="카운팅"/>
-                            <FormControlLabel control={<Checkbox/>} label="다이버전스"/>
-                            <FormControlLabel control={<Checkbox/>} label="매물대"/>
-                            <FormControlLabel control={<Checkbox/>} label="피보나치"/>
-                            <FormControlLabel control={<Checkbox/>} label="지지/저항"/>
-                            <FormControlLabel control={<Checkbox/>} label="추세선"/>
-                            <FormControlLabel control={<Checkbox/>} label="캔들패턴"/>
+                            {basisList.map((basis) =>
+                                <FormControlLabel
+                                    key={basis.code}
+                                    label={basis.name}
+                                    control={<Checkbox
+                                                value={basis.code}
+                                                checked={basis.check}
+                                                onChange={(e) => {
+                                                    let newBasisList = []
+                                                    for (let i = 0; i < basisList.length; i++) {
+                                                        if(basisList[i].code !== e.target.value) {
+                                                            newBasisList.push(basisList[i])
+                                                        } else {
+                                                            newBasisList.push({
+                                                                'code': basis.code,
+                                                                'name': basis.name,
+                                                                'check': !basis.check
+                                                            });
+                                                        }
+                                                    }
+                                                    setBasisList(newBasisList);
+                                                }}
+                                            />}
+                                />)
+                            }
                         </FormControl>
                     </Grid>
                     <Grid item>
-                        <InputLabel id="demo-row-radio-buttons-group-label"
+                        <InputLabel id="result-label"
                                     sx={{fontWeight: "bold", padding: "0.5rem 0rem"}}>결과</InputLabel>
                         <RadioGroup
                             row
-                            aria-labelledby="demo-row-radio-buttons-group-label"
-                            name="row-radio-buttons-group"
+                            name="result-buttons-group"
+                            value={result}
+                            onChange={(event) => setResult(event.target.value)}
                         >
-                            <FormControlLabel value="WAITING" control={<Radio/>} label="대기"/>
-                            <FormControlLabel value="WIN" control={<Radio/>} label="승"/>
-                            <FormControlLabel value="LOSE" control={<Radio/>} label="패"/>
+                            <FormControlLabel control={<Radio value="WAITING" />} label="대기"/>
+                            <FormControlLabel control={<Radio value="WIN" />} label="승"/>
+                            <FormControlLabel control={<Radio value="LOSE" />} label="패"/>
                         </RadioGroup>
                     </Grid>
                 </Grid>
@@ -92,7 +144,7 @@ function Plan() {
                     <ChartExplainList title={"매매 실행"}/>
                 </Grid>
                 <Grid item container md={12}>
-                    <Grid md={4}>
+                    <Grid item md={4}>
                         <TextField
                             type="number"
                             id="outlined-required"
@@ -111,7 +163,7 @@ function Plan() {
                             }}
                         />
                     </Grid>
-                    <Grid md={4}>
+                    <Grid item md={4}>
                         <TextField
                             type="number"
                             id="outlined-required"
@@ -130,7 +182,7 @@ function Plan() {
                             }}
                         />
                     </Grid>
-                    <Grid md={4}>
+                    <Grid item md={4}>
                         <TextField
                             type="number"
                             id="outlined-required"
@@ -151,7 +203,7 @@ function Plan() {
                     </Grid>
                 </Grid>
                 <Grid item container md={12}>
-                    <Grid md={6}>
+                    <Grid item md={6}>
                         <h3>잘한 점</h3>
                         <div style={{padding: "1rem 1rem"}}>
                             <TextField
@@ -162,7 +214,7 @@ function Plan() {
                             />
                         </div>
                     </Grid>
-                    <Grid md={6}>
+                    <Grid item md={6}>
                         <h3>부족한 점</h3>
                         <div style={{padding: "1rem 1rem"}}>
                             <TextField
